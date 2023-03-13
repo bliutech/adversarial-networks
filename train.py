a728b350158dbc0e3b17d771a75e02d4b317ea6f
Benson Liu <bensonhliu@gmail.com>
2023-03-13 03:42:31 -0700

    updated train.py and improve models/cnn.pth to 72% accuracy * implemented data augmentation with horizontal flips (tested additional data augmentation such as RandomRotation and RandomAffine but RandomHorizontalFlips alone seemed best due to CIFAR-10 nature) * updated .gitignore for ipynb checkpoints * added training and testing accuracies and loss reporting during training * updated file paths for saving checkpoints

--- train.py
+++ train.py
@@ -2,6 +2,8 @@
 import torch
 from torchvision import datasets, transforms
 
+import numpy as np
+
 from tqdm import tqdm
 
 from networks.cnn import CNN
@@ -13,19 +15,25 @@ transform = transforms.Compose([transforms.ToTensor()])
 
 # Download CIFAR10 dataset
 train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 # Create data loaders
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
+
+data_augmentation = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+])
 
 # Create the model
 model = CNN()
 model.to(device)
 
+EPOCHS = 10
+
 # Define the loss function and optimizer
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-EPOCHS = 10
 
 progress = tqdm(total=len(train_loader)*EPOCHS, desc="Training")
 
@@ -35,6 +43,9 @@ for epoch in range(EPOCHS):
         model.train()
         images = images.to(device)
         labels = labels.to(device)
+
+        # perform data augmentation
+        images = data_augmentation(images)
 
         # Forward pass
         outputs = model(images)
@@ -46,8 +57,42 @@ for epoch in range(EPOCHS):
         optimizer.step()
 
         progress.update(1)
-    
-    progress.write(f'Epoch [{epoch+1}/{10}], Loss: {loss.item():.4f}')
+
+    # currently for every epoch but can adjust to be les frequent
+    # calculate train and test loss and accuracy
+    if epoch % 1 == 0:
+        train_losses = []
+        train_accuracies = []
+
+        with torch.no_grad():
+            for inputs, labels in train_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                accuracy = (torch.max(outputs, dim=1)[1] == labels).to(torch.float32).mean()
+                train_losses.append(loss.cpu().numpy())
+                train_accuracies.append(accuracy.cpu().numpy())
+
+        train_loss, train_accuracy = np.mean(train_losses), np.mean(train_accuracies)
+
+
+        test_losses = []
+        test_accuracies = []
+
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                accuracy = (torch.max(outputs, dim=1)[1] == labels).to(torch.float32).mean()
+                test_losses.append(loss.cpu().numpy())
+                test_accuracies.append(accuracy.cpu().numpy())
+
+        test_loss, test_accuracy = np.mean(test_losses), np.mean(test_accuracies)
+
+        progress.write(f"{('[' + str(epoch + 1) + ']'):8s}   Train: {str(train_accuracy * 100):.6}% ({str(train_loss):.6})   Test: {str(test_accuracy * 100):.6}% ({str(test_loss):.6})")
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.pth')
+torch.save(model.state_dict(), 'models/cnn.pth')
