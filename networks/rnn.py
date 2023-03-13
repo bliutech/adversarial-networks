16c84e12a0600c1b5b177d436935eef6a5b8d73f
Benson Liu <bensonhliu@gmail.com>
2023-03-13 01:10:56 -0700

    updated repo configuration and outline * moved pretrained models to models/ * added template for rnn.py and FGSMTransforms for data augmentation in fgsm.py * updated requirements.txt with appropriate version * updated main.ipynb to reflect new repository structure and test outputs

---
+++ networks/rnn.py
@@ -0,0 +1,9 @@
+from torch import nn
+
+class RNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        return NotImplementedError
+
+    def forward(self, input):
+        return NotImplementedError
