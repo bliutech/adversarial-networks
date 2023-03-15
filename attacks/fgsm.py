971499f69340ceefd2962b964c5b6247a9b16a15
isabellaqian <isabellaqian@hotmail.com>
2023-03-15 05:51:04 -0700

    progress in fgsm transformation (custom class, override dataset, batchsize=1 pretrained cnn)

--- attacks/fgsm.py
+++ attacks/fgsm.py
@@ -6,41 +6,38 @@ import torchvision.transforms.functional as TF
 
 from ..networks.cnn import CNN
 
-def fgsm_attack(image, episilon, data_grad):
-    # Collect the element-wise sign of the data gradient
-    sign_data_grad = data_grad.sign()
-    # Create the perturbed image by adjusting each pixel of the input image
-    perturbed_image = image + episilon*sign_data_grad
-    # Adding clipping to maintain [0,1] range
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    # Return the perturbed image
-    return perturbed_image
 
-class FGSMTransform:
+class FGSMTransform1:
     """Perform a fast gradient sign attack on an image."""
-    def __init__(self, epsilon):
-        self.epsilon = epsilon
 
-    def __call__(self, image):
-        # Set requires_grad attribute of tensor. Important for Attack
-        model = CNN().to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-        model.load_state_dict(torch.load('../models/cnn.pth'))
-        image.requires_grad = True
-        # Forward pass the data through the model
-        output = model(image)
-        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        # If the initial prediction is wrong, dont bother attacking, just return the initial image
-        # if init_pred.item() != target.item():
-        #     return image
-        # Calculate the loss
-        # loss = F.nll_loss(output, target)
-        # Zero all existing gradients
-        # model.zero_grad()
-        # Calculate gradients of model in backward pass
-        # loss.backward()
-        # Collect datagrad
-        data_grad = image.grad.data
-        # Call FGSM Attack
-        perturbed_image = fgsm_attack(image, self.epsilon, data_grad)
-        # Return the perturbed image
-        return perturbed_image
+    def __init__(self, epsilon=0.005):
+        self.epsilon = epsilon
+        self.model = CNN().to(
+            torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        )
+        self.model.load_state_dict(torch.load("./models/cnn_batchsize1.pth"))
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+
+    def __call__(self, x):
+        # if we process a single image instead of batch, we need to add a fourth dimension
+        if x.dim() == 3:
+            x = x.unsqueeze(0)
+        x = x.to("cuda" if torch.cuda.is_available() else "cpu")
+        x.requires_grad = True
+
+        # forward, backward pass to calculate gradient
+        output = self.model(x)
+        loss = self.criterion(output, labels)
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        # Collect gradients
+        data_grad = x.grad.data
+
+        # Call FGSM Attack, same as fgsm()
+        sign_data_grad = data_grad.sign()
+        perturbed_images = x + self.epsilon * sign_data_grad
+        perturbed_images = torch.clamp(perturbed_images, 0, 1)
+        print(perturbed_images.size())
+        return perturbed_images
